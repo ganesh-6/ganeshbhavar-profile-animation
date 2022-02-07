bbef2b9d2cc4a9fd7bf76d8919ff93027ecd2a28
ganesh bhavar <85879976+ganesh-347@users.noreply.github.com>
2022-02-07 11:23:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Wlecome to my personal website",""],
+        strings: ["Welcome to my personal website",""],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
